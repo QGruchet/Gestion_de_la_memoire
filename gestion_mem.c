8b6ddf8c45e340b8040cc45a3325d5a2ced72b79
QGruchet <55387771+QGruchet@users.noreply.github.com>
2020-04-27 20:04:17 +0200

    V2

--- gestion_mem.c
+++ gestion_mem.c
@@ -6,6 +6,8 @@
 #include <fcntl.h>
 #include "se_fichier.c"
 
+void * thread_fils(void * arg);
+
 /*******************CONTIENT LES INFOS DU FICHIER .CFG****************/
 typedef struct {
 	int nombre_frames;
@@ -21,7 +23,15 @@ typedef struct{
 	int *memoire_lente;
 }TYPE_MEMOIRE;
 
-/****************LECTURE DU FICHIER*********************/
+/*****************STRUCTURE DES MUTEX*******************************/
+typedef struct {
+	pthread_mutex_t *mutex;
+	pthread_cond_t *cond;
+	int * compt;
+	int nombre_thread;
+} barriere_t;
+
+/****************INTITIALISATION DE MEMOIRE_CONFIG*********************/
 MEMOIRE_CONFIG _init_cfg(char* nomFic){
 
 	MEMOIRE_CONFIG m_config;
@@ -51,7 +61,7 @@ MEMOIRE_CONFIG _init_cfg(char* nomFic){
 	if(SE_fermeture(f) == -1){
 		erreur = 7;
 	}
-	
+
 	if(erreur != 0){
 		printf("Erreur lors de la lecture du fichier de configuration. (Erreur n°%d)\n", erreur);
 		exit(0);
@@ -61,16 +71,20 @@ MEMOIRE_CONFIG _init_cfg(char* nomFic){
 	return m_config;
 }
 
-/***************INITIALISATION TYPE DE MEMOIRE***********************/
-TYPE_MEMOIRE _init_type_(){
+/***************INITIALISATION TYPE_MEMOIRE***********************/
+TYPE_MEMOIRE _init_type_(MEMOIRE_CONFIG m_config){
 	TYPE_MEMOIRE type;
 	type.memoire_lente = malloc(sizeof(int) * 10);
-	type.memoire_rapide = malloc(sizeof(int) * 10);
+	type.memoire_rapide = malloc(sizeof(int) * m_config.nombre_frames);
+
+	for (int i = 0; i < m_config.nombre_frames; ++i)
+	{
+		type.memoire_rapide[i] = i + 1;
+	}
 
 	for (int i = 0; i < 10; ++i)
 	{
-		type.memoire_rapide[i] = i + 10;
-		type.memoire_lente[i] = i;
+		type.memoire_lente[i] = i + 10;
 	}
 
 	return type;
@@ -89,29 +103,81 @@ void affiche_struct(MEMOIRE_CONFIG m_config){
 /**************AFFICHAGE DE LA STRUCT TYPE_MEMOIRE*****************/
 void affiche_type(TYPE_MEMOIRE type){
 
-	printf("memoire_rapide :");
-	for (int i = 0; i < 10; ++i)
-	{
-		printf("%3d", type.memoire_rapide[i]);
+	printf("(TEST)memoire_rapide : ");
+	for(int i = 0; i < 4; ++i){
+		printf("%d ", type.memoire_rapide[i]);
 	}
 	printf("\n");
-	printf("memoire_lente :");
+	printf("(TEST)memoire_lente :");
 	for (int i = 0; i < 10; ++i)
 	{
-		printf("%2d", type.memoire_lente[i]);
+		printf("%2d ", type.memoire_lente[i]);
 	}
 	printf("\n");
 }
 
-/**************OBTIENT L'ADRESSE PHYSIQUE*****************/
-int get_adr_phy(MEMOIRE_CONFIG m_config){
+/**************OBTIENT LES ADRESSES*****************/
+int* get_adr(MEMOIRE_CONFIG m_config){
 
-	int adr_phy = 0;
-	adr_phy = m_config.taille_page * m_config.nombre_page ;
-
-	return adr_phy;
+	int* adresse = malloc(sizeof(int) * 10);
+	//adresse[i] = adresse logiques et i = adresse physiques
+	for (int i = 0; i < 10; ++i)
+	{
+		adresse[i] = m_config.nombre_frames * m_config.taille_page + (i % m_config.nombre_frames);
+	}
+	return adresse;
 }
 
+/*******************AFFICHAGE DES ADRESSES***************************/
+void affiche_adresse(int* adresse){
+
+	for (int i = 0; i < 10; ++i){
+		printf("Adresse physique : %d \t", i);
+		printf("Adresse logique : %d\n", adresse[i]);
+	}
+}
+
+/****************CREATION DES THREADS****************/
+void create_threads(MEMOIRE_CONFIG m_config){
+	pthread_t tid[m_config.nombre_threads];
+	barriere_t bar[m_config.nombre_threads];
+	pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+	pthread_cond_t cnd = PTHREAD_COND_INITIALIZER;
+	int compt;
+
+	compt = m_config.nombre_threads;
+	printf("(TEST)Sortie thread fils :\n");
+	for (int i = 0; i < m_config.nombre_threads; ++i)
+	{
+		bar[i].compt = &compt;
+		bar[i].mutex = &mut;
+		bar[i].cond = &cnd;
+		pthread_create(tid + i, NULL, thread_fils, bar + i);
+	}
+	for (int i = 0; i < m_config.nombre_threads; ++i){
+		pthread_join(tid[i], NULL);
+	}
+	printf("\n");
+}
+
+void * thread_fils(void * arg){
+	barriere_t *bar = (barriere_t *)arg;
+	
+	pthread_mutex_lock(bar->mutex);
+	printf("  >> Entrer barriere\n");
+	(*bar->compt)--;
+	if(*bar->compt != 0){
+		pthread_cond_wait(bar->cond, bar->mutex);
+	}
+	else{
+		pthread_cond_broadcast(bar->cond);
+	}
+	printf("  << Sortie barriere\n");
+	pthread_mutex_unlock(bar->mutex);
+
+	return NULL;
+}
+		
 /******************LIBERATION MEMOIRE*****************/
 void free_type(TYPE_MEMOIRE type){
 	free(type.memoire_lente);
@@ -120,13 +186,14 @@ void free_type(TYPE_MEMOIRE type){
 
 /************PROGRAMME PRINCIPALE***************/
 int main(void){
-	int adr_phy = 0;
+	int* adresse = malloc(sizeof(int) * 10);
 
 	MEMOIRE_CONFIG m_config= _init_cfg("config.cfg");
-	TYPE_MEMOIRE type = _init_type_();
+	TYPE_MEMOIRE type = _init_type_(m_config);
 	affiche_struct(m_config);
-	adr_phy = get_adr_phy(m_config);
-	printf("adr_phy : 0 -> %d\n", adr_phy);
+	adresse = get_adr(m_config);
+	affiche_adresse(adresse);
+	create_threads(m_config);
 	affiche_type(type);
 	free_type(type);
 	
